98a839712da87c1d67b930100ca991af8cda601b
NaritaShosei <onigirikun7171@gmail.com>
2025-08-15 15:39:26 +0900

    [add]カメラマネージャー

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [HideInInspector] public Camera MainCamera;
+    private void Awake()
+    {
+        ServiceLocator.Set(this);
+        MainCamera = Camera.main;
+    }
+}
