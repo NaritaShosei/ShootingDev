be20fb15649147fd44018a5cd72e3ab50300fe65
NaritaShosei <onigirikun7171@gmail.com>
2025-08-15 15:38:57 +0900

    [add]InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    private InputActions _input; // 自動生成クラス
+    public event Action<Vector2> OnMove;
+    public event Action OnAttack;
+
+    private void Awake()
+    {
+        ServiceLocator.Set(this);
+        _input = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+
+        _input.Player.Move.performed += ctx => OnMove?.Invoke(ctx.ReadValue<Vector2>());
+        _input.Player.Move.canceled += ctx => OnMove?.Invoke(Vector2.zero);
+
+        _input.Player.Attack.performed += ctx => OnAttack?.Invoke();
+        _input.Player.Attack.canceled += ctx => OnAttack?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    public Vector2 GetMoveInput()
+    {
+        return _input.Player.Move.ReadValue<Vector2>();
+    }
+}
