ae2c16f40a8b5b969aa3b18efef325e1ef7abaae
NaritaShosei <onigirikun7171@gmail.com>
2025-08-15 20:29:12 +0900

    [update]武器データの改修

--- Assets/Scripts/Weapon/WeaponData.cs
+++ Assets/Scripts/Weapon/WeaponData.cs
@@ -3,7 +3,24 @@
 [CreateAssetMenu(menuName = "WeaponData", fileName = "WeaponData")]
 public class WeaponData : ScriptableObject
 {
-    [SerializeField] private WeaponBase _weapon;
+    [Header("基本情報")]
+    [SerializeField] private int _weaponID;
+    [SerializeField] private string _weaponName;
+    [SerializeField] private Sprite _weaponIcon;
+
+    [Header("性能")]
     [SerializeField] private float _attackPower = 1;
     [SerializeField] private float _attackRate = 1;
+    [SerializeField] private float _range;
+
+    [Header("プレハブ")]
+    [SerializeField] private GameObject _weaponPrefab;
+    public int WeaponID => _weaponID;
+    public string WeaponName => _weaponName;
+    public Sprite WeaponIcon => _weaponIcon;
+    public float AttackPower => _attackPower;
+    public float AttackRate => _attackRate;
+    public float Range => _range;
+    public GameObject WeaponPrefab => _weaponPrefab;
+
 }
