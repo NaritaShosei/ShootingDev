20a44887fd578645f3ab32deb356da94008708bf
NaritaShosei <onigirikun7171@gmail.com>
2025-08-15 15:39:42 +0900

    [add]Playerの移動

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField, Range(0, 1)] private float _moveLimit = 0.3f;
+    private Camera _camera;
+    private Vector3 _input;
+    private void Start()
+    {
+        _camera = ServiceLocator.Get<CameraManager>().MainCamera;
+        ServiceLocator.Get<InputManager>().OnMove += Movement;
+    }
+    private void Update()
+    {
+        transform.position += _input * _moveSpeed * Time.deltaTime;
+    }
+
+    private void Movement(Vector2 moveInput)
+    {
+        _input = moveInput;
+    }
+}
