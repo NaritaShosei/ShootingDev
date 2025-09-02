2094a90ec3166fd5599c07d4275ec92f1144afb3
NaritaShosei <onigirikun7171@gmail.com>
2025-09-02 23:46:00 +0900

    [add]Player管理クラス

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, IPlayerHealth
+{
+    [SerializeField] private float _maxHealth;
+    private float _currentHealth;
+
+    public float Health => _currentHealth;
+
+    public float MaxHealth => _maxHealth;
+
+    public bool IsAlive => Health > 0;
+
+    private void Awake()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void Heal(float amount)
+    {
+        _currentHealth += amount;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+    }
+}
